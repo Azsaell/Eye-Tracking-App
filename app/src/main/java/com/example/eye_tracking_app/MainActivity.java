234854f26467ecb9549f53f1a20ece79c9b2ea0c
Azsael <56028507+Azsaell@users.noreply.github.com>
2023-04-11 16:49:05 +0200

    gówno

--- app/src/main/java/com/example/eye_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/eye_tracking_app/MainActivity.java
@@ -1,48 +1,90 @@
 package com.example.eye_tracking_app;
 
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.eye_tracking_app.databinding.ActivityMainBinding;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.face.FaceLandmark;
+import com.google.mlkit.vision.interfaces.Detector;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    private static final String TAG = "MainActivity";
+//    VideoView videoView;
+    EditText textView;
+
+    //For looking logs
+    ArrayAdapter adapter;
+    ArrayList<String> list = new ArrayList<>();
+
+    CameraXSource cameraSource;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        textView = (EditText) findViewById(R.id.editText);
+        textView.setText("Dupa testowa");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 1);
+            Toast.makeText(this, "Grant Permission and restart app", Toast.LENGTH_SHORT).show();
+        }
+        else {
+//            videoView = findViewById(R.id.videoView);
+            textView = findViewById(R.id.editText);
+            adapter = new ArrayAdapter<>(this,   android.R.layout.simple_list_item_1, list);
+            //videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback));
+            //videoView.start();
+            createCameraSource();
+        }
 
-        setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        setSupportActionBar(binding.toolbar);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -72,5 +114,99 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private class EyesTracker extends Tracker<Face> {
+
+        private final float THRESHOLD = 0.75f;
+
+        private EyesTracker() {
+
+        }
+
+        @Override
+        public void onUpdate(Detector.Detections<Face> detections, Face face) {
+            if (face.getIsLeftEyeOpenProbability() > THRESHOLD || face.getIsRightEyeOpenProbability() > THRESHOLD) {
+                Log.i(TAG, "onUpdate: Eyes Detected");
+                FaceLandmark leftEar = face.getLandmark(FaceLandmark.LEFT_EAR);
+                showStatus("Eyes Detected and open, so video continues");
+//                if (!videoView.isPlaying())
+//                    videoView.start();
+
+            }
+            else {
+//                if (videoView.isPlaying())
+//                    videoView.pause();
+
+                showStatus("Eyes Detected and closed, so video paused");
+            }
+        }
+
+        @Override
+        public void onMissing(Detector.De<Face> detections) {
+            super.onMissing(detections);
+            showStatus("Face Not Detected yet!");
+        }
+
+        @Override
+        public void onDone() {
+            super.onDone();
+        }
+    }
+
+    private class FaceTrackerFactory implements MultiProcessor.Factory<Face> {
+
+        private FaceTrackerFactory() {
+
+        }
+
+        @Override
+        public Tracker<Face> create(Face face) {
+            return new EyesTracker();
+        }
+    }
+
+    public void createCameraSource() {
+        FaceDetectorOptions options = new FaceDetectorOptions.Builder()
+                .enableTracking()
+                .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                .build();
+        FaceDetector detector = FaceDetection.getClient(options);
+//        detector.
+//        detector.setProcessor(new MultiProcessor.Builder(new FaceTrackerFactory()).build());
+
+
+        cameraSource = new CameraXSource.Builder(this, detector)
+                .setRequestedPreviewSize(1024, 768)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(30.0f)
+                .build();
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            cameraSource.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showStatus(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView = findViewById(R.id.editText);
+                textView.setText(message);
+            }
+        });
     }
 }
